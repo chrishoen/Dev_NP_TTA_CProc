341ec8b310325e2ee32fe60305d5ebff0c91fe9e
chrishoen <chris123hoen@gmail.com>
2020-12-24 17:00:41 -0500

    added msg defs

--- CommonLib/sxMsgDefs.cpp
+++ CommonLib/sxMsgDefs.cpp
@@ -3,26 +3,29 @@
 //******************************************************************************
 #include "stdafx.h"
 
-#include "evtMsgDefs.h"
+#include "sxMsgDefs.h"
 
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
 
-namespace Evt
+namespace SX
 {
 
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
-// Return an event record variable as a string.
+// Get a message ident as a string.
 
-char* get_EvtType_asString(int aType)
+char* get_MsgId_asString(int aMsgId)
 {
-   switch (aType)
+   switch (aMsgId)
    {
-   case cEvt_Type1: return "Type1";
-   case cEvt_Type2: return "Type2";
+      case cMsgId_tst: return "tst";
+      case cMsgId_gsv: return "gsv";
+      case cMsgId_gbc: return "gbc";
+      case cMsgId_gft: return "gft";
+      case cMsgId_gsx: return "gsx";
    }
    return "UNKNOWN";
 }
@@ -30,72 +33,16 @@ char* get_EvtType_asString(int aType)
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
-// Return an event record variable as a string.
+// Get a message ident as an integer.
 
-char* get_EvtSeverity_asString(int aSeverity)
+int get_MsgId_asInt(const char* aMsgId)
 {
-   switch (aSeverity)
-   {
-   case cEvt_SeverityUseDefault: return "UseDefault";
-   case cEvt_SeverityInfo: return "Info";
-   case cEvt_SeverityWarning: return "Warning";
-   case cEvt_SeveritySevere: return "Severe";
-   case cEvt_SeverityCritical: return "Critical";
-   }
-   return "UNKNOWN";
-}
-
-
-//******************************************************************************
-//******************************************************************************
-//******************************************************************************
-// Return true if the event can cause an alarm.
-
-bool isAlarmable(int aSeverity)
-{
-   switch (aSeverity)
-   {
-   case cEvt_SeverityUseDefault: return false;
-   case cEvt_SeverityInfo: return false;
-   case cEvt_SeverityWarning: return false;
-   case cEvt_SeveritySevere: return true;
-   case cEvt_SeverityCritical: return true;
-   }
-   return false;
-}
-
-//******************************************************************************
-//******************************************************************************
-//******************************************************************************
-// Return a partition code as a string.
-
-char* get_EvtSource_asString(int aSource)
-{
-   switch (aSource)
-   {
-   case cEvt_SourceTest: return "Test";
-   case cEvt_SourceTTA: return "TTA";
-   case cEvt_SourceDA: return "DA";
-   case cEvt_SourceCU: return "CU";
-   case cEvt_SourceHLC: return "HLC";
-   case cEvt_SourceAux: return "Aux";
-   }
-   return "UNKNOWN";
-}
-
-
-//******************************************************************************
-//******************************************************************************
-//******************************************************************************
-// Get the timestamp as a string. This is passed in a local buffer.
-
-char* get_timespec_asString(timespec aTimeSpec, char* aBuffer)
-{
-   char tTemp[40];
-   strftime(tTemp, 40, "%F %T", localtime(&aTimeSpec.tv_sec));
-   sprintf(aBuffer, "%s.%03ld", tTemp, aTimeSpec.tv_nsec / 1000000);
-
-   return aBuffer;
+   if (strcmp(aMsgId, get_MsgId_asString(cMsgId_tst)) == 0) return cMsgId_tst;
+   if (strcmp(aMsgId, get_MsgId_asString(cMsgId_gsv)) == 0) return cMsgId_gsv;
+   if (strcmp(aMsgId, get_MsgId_asString(cMsgId_gbc)) == 0) return cMsgId_gbc;
+   if (strcmp(aMsgId, get_MsgId_asString(cMsgId_gft)) == 0) return cMsgId_gft;
+   if (strcmp(aMsgId, get_MsgId_asString(cMsgId_gsx)) == 0) return cMsgId_gsx;
+   return 0;
 }
 
 //******************************************************************************
