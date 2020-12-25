02f7c63d94b09d21a0c40976a210bc8e3ba3074e
chrishoen <chris123hoen@gmail.com>
2020-12-25 14:25:08 -0500

    cmake stuff

--- MyGlobalImports.cmake
+++ MyGlobalImports.cmake
@@ -4,26 +4,14 @@
 
 function(my_init_global_import_variables)
 
-   if(MSVC)
-      set (MyRisLibIncludePath "C:\\MyTools\\MyLib\\include\\RisLib" PARENT_SCOPE)
-      set (MyRisLibImportPath  "C:\\MyTools\\MyLib\\lib\\RisLib.lib" PARENT_SCOPE)
-      set (MyDspLibIncludePath "C:\\MyTools\\MyLib\\include\\DspLib" PARENT_SCOPE)
-      set (MyDspLibImportPath  "C:\\MyTools\\MyLib\\lib\\DspLib.lib" PARENT_SCOPE)
-   elseif(NOT CMAKE_SYSTEM_VERSION EQUAL 101)
-      set (MyRisLibIncludePath "/usr/local/include/RisLib" PARENT_SCOPE)
-      set (MyRisLibImportPath  "/usr/local/lib/libRisLib.so" PARENT_SCOPE)
-      set (MyDspLibIncludePath "/usr/local/include/DspLib" PARENT_SCOPE)
-      set (MyDspLibImportPath  "/usr/local/lib/libDspLib.a" PARENT_SCOPE)
-      set (MyPhpCppIncludePath "/usr/include" PARENT_SCOPE)
-      set (MyPhpCppImportPath  "/usr/lib/libphpcpp.so" PARENT_SCOPE)
-   else()
-      set (MyRisLibIncludePath "C:/MyTools/MyLib/include/RisLib" PARENT_SCOPE)
-      set (MyRisLibImportPath  "C:/MyTools/MyLib/lib/libRisLib.so" PARENT_SCOPE)
-      set (MyDspLibIncludePath "C:/MyTools/MyLib/include/DspLib" PARENT_SCOPE)
-      set (MyDspLibImportPath  "C:/MyTools/MyLib/lib/libDspLib.a" PARENT_SCOPE)
-      set (MyPhpCppIncludePath "C:/Beagle/phpcpp/include" PARENT_SCOPE)
-      set (MyPhpCppImportPath  "C:/Beagle/phpcpp/lib/libphpcpp.so" PARENT_SCOPE)
-   endif()
+   set (MyRisLibIncludePath "C:/MyTools/MyLib/include/RisLib" PARENT_SCOPE)
+   set (MyRisLibImportPath  "C:/MyTools/MyLib/lib/libRisLib.so" PARENT_SCOPE)
+   set (MyDspLibIncludePath "C:/MyTools/MyLib/include/DspLib" PARENT_SCOPE)
+   set (MyDspLibImportPath  "C:/MyTools/MyLib/lib/libDspLib.a" PARENT_SCOPE)
+   set (MyShareLibIncludePath "C:/MyTools/MyLib/include/ShareLib" PARENT_SCOPE)
+   set (MyShareLibImportPath  "C:/MyTools/MyLib/lib/libShareLib.so" PARENT_SCOPE)
+   set (MyEventLibIncludePath "C:/MyTools/MyLib/include/EventLib" PARENT_SCOPE)
+   set (MyEventLibImportPath  "C:/MyTools/MyLib/lib/libEventLib.so" PARENT_SCOPE)
 
 endfunction()
 
@@ -33,17 +21,9 @@ endfunction()
 
 function(my_lib_import_RisLib _target)
 
-   if (MSVC)
-      add_library(RisLib STATIC IMPORTED)
-      set_target_properties(RisLib PROPERTIES IMPORTED_LOCATION ${MyRisLibImportPath})
-      target_link_libraries(RisLib INTERFACE ws2_32)
-      target_link_libraries(RisLib INTERFACE winmm)
-      target_link_libraries(${_target} RisLib)
-   else()
-      add_library(RisLib SHARED IMPORTED)
-      set_target_properties(RisLib PROPERTIES IMPORTED_LOCATION ${MyRisLibImportPath})
-      target_link_libraries(${_target} RisLib)
-   endif()
+   add_library(RisLib SHARED IMPORTED)
+   set_target_properties(RisLib PROPERTIES IMPORTED_LOCATION ${MyRisLibImportPath})
+   target_link_libraries(${_target} RisLib)
 
 endfunction()
 
@@ -59,7 +39,7 @@ endfunction()
 
 function(my_lib_import_DspLib _target)
 
-   add_library(DspLib STATIC IMPORTED)
+   add_library(DspLib SHARED IMPORTED)
    set_target_properties(DspLib PROPERTIES IMPORTED_LOCATION ${MyDspLibImportPath})
    target_link_libraries(${_target} DspLib)
 
@@ -75,21 +55,40 @@ endfunction()
 #*******************************************************************************
 #*******************************************************************************
 
-function(my_lib_import_phpcpp _target)
+function(my_lib_import_ShareLib _target)
 
-   add_library(phpcpp SHARED IMPORTED)
-   set_target_properties(phpcpp PROPERTIES IMPORTED_LOCATION ${MyPhpCppImportPath})
-   target_link_libraries(${_target} phpcpp)
-   
+   add_library(ShareLib SHARED IMPORTED)
+   set_target_properties(ShareLib PROPERTIES IMPORTED_LOCATION ${MyShareLibImportPath})
+
+   target_link_libraries(${_target} ShareLib)
+
 endfunction()
 
-function(my_inc_import_phpcpp _target)
+function(my_inc_import_ShareLib _target)
 
-   target_include_directories(${_target} PUBLIC ${MyPhpCppIncludePath})
+   target_include_directories(${_target} PUBLIC ${MyShareLibIncludePath})
 
 endfunction()
 
 #*******************************************************************************
+#*******************************************************************************
+#*******************************************************************************
+
+function(my_lib_import_EventLib _target)
+
+   add_library(EventLib SHARED IMPORTED)
+   set_target_properties(EventLib PROPERTIES IMPORTED_LOCATION ${MyEventLibImportPath})
+
+   target_link_libraries(${_target} EventLib)
+
+endfunction()
+
+function(my_inc_import_EventLib _target)
+
+   target_include_directories(${_target} PUBLIC ${MyEventLibIncludePath})
+
+endfunction()
+
 #*******************************************************************************
 #*******************************************************************************
 #*******************************************************************************
